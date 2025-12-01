69094bcbf06220af803a1fcf4d75e76943cc3faf
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-01 16:03:24 -0500

    new archive for create DB

--- database.py
+++ database.py
@@ -8,20 +8,32 @@ cursor = conexion.cursor()
 
 # for create a table
 cursor.execute("""
+    CREATE TABLE IF NOT EXISTS companies (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        api_key TEXT NOT NULL
+    );
+""")
+
+cursor.execute("""
     CREATE TABLE IF NOT EXISTS qrs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name_logo TEXT NOT NULL,
-        name_qr TEXT NOT NULL,
+        tenant_id INTEGER NOT NULL,
+        logo_name TEXT NOT NULL,
+        qr_name TEXT NOT NULL,
         destination_url TEXT NOT NULL,
         server_url TEXT NOT NULL,
-        unique_id TEXT NOT NULL
+        unique_id TEXT NOT NULL UNIQUE,
+               
+        FOREIGN KEY (tenant_id) REFERENCES companies(id)
+            ON DELETE CASCADE
     );
 """)
 
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS users_scam (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        qr_id INTEGER NOT NULL,
+        id_unique TEXT NOT NULL,
         ip TEXT NOT NULL,
         device TEXT,
         country TEXT,
@@ -30,7 +42,21 @@ cursor.execute("""
         longitude REAL,
         datetime TEXT DEFAULT CURRENT_TIMESTAMP,
         
-        FOREIGN KEY (qr_id) REFERENCES qrs(id)
+        FOREIGN KEY (id_unique) REFERENCES qrs(unique_id)
+            ON DELETE CASCADE
+    );
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS geo_registers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id_users_scam INTEGER NOT NULL,
+        country TEXT,
+        city TEXT,
+        latitude REAL,
+        longitude REAL,
+        
+        FOREIGN KEY (id_users_scam) REFERENCES users_scam(id)
             ON DELETE CASCADE
     );
 """)
