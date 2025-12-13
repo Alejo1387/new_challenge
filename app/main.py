7d9b238118598946a1e71752e4db425a04bd46c1
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-13 14:50:26 -0500

    divided APIs into routers

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from app.APIs.qr_create import router as qr_create_router
 from app.APIs.qr_scan import router as qr_scan_router
+from app.APIs.save_logo import router as save_logo_router
 
 
 app = FastAPI()
 
 # For create QR codes
-app.include_router(qr_create_router, prefix="/qr", tags=["QR Create"])
+app.include_router(qr_create_router)
 
 # for scan QR codes
-app.include_router(qr_scan_router, prefix="/qr", tags=["QR Scan"])
+app.include_router(qr_scan_router)
+
+# for save logo files
+app.include_router(save_logo_router)
