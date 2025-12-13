b2170980ab667b08353068ef2fd13757fd900027
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-13 14:47:31 -0500

    create for save image and save name in DB

---
+++ app/services/local_storage.py
@@ -0,0 +1,32 @@
+from fastapi import UploadFile
+from app.utils.create_name_file import create_unique
+from app.core.database import SessionLocal
+from sqlalchemy import text
+
+upload_dir = "app/logos"
+
+async def save_file(file: UploadFile, filename: str, tenant_id: str):
+    unique_id = create_unique()
+
+    extencion = filename.split(".")[-1]
+
+    new_name = f"{unique_id}.{extencion}"
+
+    file_location = f"{upload_dir}/{new_name}"
+
+    content = await file.read()
+
+    with open(file_location, "wb+") as f:
+        f.write(content)
+    
+    db = SessionLocal()
+    
+    try:
+        db.execute(text("""
+            INSERT INTO logos (id, tenant_id) VALUES (:id, :tenant_id)
+        """), {"id": new_name, "tenant_id": tenant_id})
+        db.commit()
+    finally:
+        db.close()
+
+    return new_name
