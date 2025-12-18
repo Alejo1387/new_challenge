84d172a515f7bf39d9e130709faf6bcb30f63b38
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-18 17:47:46 -0500

    Add data for Qr

--- app/models/Qr_model.py
+++ app/models/Qr_model.py
@@ -1,6 +1,22 @@
 from pydantic import BaseModel
+from typing import Optional
 
 # for get data on body request
 class QRData(BaseModel):
     url: str
     logoname: str = None
+
+class QRCreate1(BaseModel):
+    company_id: int
+    url: str
+    logoname: Optional[str] = None
+    storage: str
+
+class QRCreate2(BaseModel):
+    company_id: int
+    url: str
+    logoname: Optional[str] = None
+    storage: str
+    fileName: str
+    server_url: str
+    logo_storage: str
