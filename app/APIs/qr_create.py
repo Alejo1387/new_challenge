10926b896a0d55c8d7cc148d4346538e6ff87b1e
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-18 17:34:36 -0500

    Change for save in local and S3

--- app/APIs/qr_create.py
+++ app/APIs/qr_create.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Request, HTTPException, Depends
 from app.models.Qr_model import QRData
+from app.models.Qr_model import QRCreate1
 from app.core.security import check_api
 from app.services.qr_generator import qrs
 from app.services.verify_logo import ver_logo
 from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from app.utils.read_s3 import read_file
 
 router = APIRouter()
+storage = "S3"
 
 @router.post("/create")
 async def create_qr(CreateData: QRData, company_id: int = Depends(check_api)):
@@ -15,14 +20,39 @@ async def create_qr(CreateData: QRData, company_id: int = Depends(check_api)):
         if not logo_exist:
             raise HTTPException(status_code=404, detail="Logo not found")
     
-    result = qrs(company_id, CreateData.url, CreateData.logoname)
+    Qrcreate = QRCreate1(
+        company_id=company_id,
+        url=CreateData.url,
+        logoname=CreateData.logoname,
+        storage=storage
+    )
+
+    
+    result = qrs(Qrcreate)
     # print(url, name, logoname)
 
-    if result == "URL don't valid":
+    if result[0] == "URL don't valid":
         raise HTTPException(status_code=400, detail="URL is not valid")
     else:
-        try:
-            file_path = f"app/img/{result}"
-            return FileResponse(path=file_path, filename=result, media_type='application/octet-stream')
-        except Exception as e:
-            return {"error": str(e)}
+        if result[0] == "Local":
+            file_relative_path = result[1]
+            file_name = file_relative_path.split("/")[-1]
+            file_path = f"app/{file_relative_path}"
+            try:
+                                                                                  # application/octet-stream
+                return FileResponse(path=file_path, filename=file_name, media_type='image/png')
+            except Exception as e:
+                return {"error": str(e)}
+        else:
+            try:
+                file_byte = read_file(result[1])
+                return StreamingResponse(
+                    BytesIO(file_byte),
+                    media_type="application/octet-stream",
+                    headers={"Content-Disposition": f"attachment; filename={result[1]}"}
+                )
+            except Exception:
+                raise HTTPException(
+                    status_code=404,
+                    detail="File not found in S3"
+                )
