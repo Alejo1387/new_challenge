2e5007ee798f92714553929ffd7a6927e16831aa
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-18 17:35:22 -0500

    Change for save in local and S3

--- app/APIs/save_logo.py
+++ app/APIs/save_logo.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Request, HTTPException, Depends, UploadFile, File
 from app.core.security import check_api
 from app.services.local_storage import save_file
+from app.services.save_logo_s3 import save_logo_s3
 
 router = APIRouter()
+storage = "Local"
 
 @router.post("/save")
 async def save_logo(file: UploadFile = File(...), tenant_id: int = Depends(check_api)):
@@ -11,9 +13,16 @@ async def save_logo(file: UploadFile = File(...), tenant_id: int = Depends(check
     
     filename = file.filename
 
-    new_logoname = await save_file(file, filename, tenant_id)
-
-    return {
-        "message" : "Logo saved successfully",
-        "name": new_logoname
-    }
+    if storage == "Local":
+        new_logoname = await save_file(file, filename, tenant_id)
+        return {
+            "message" : "Logo saved successfully",
+            "name" : f"{new_logoname}"
+        }
+    else:
+        new_logoname = await save_logo_s3(file, filename, tenant_id)
+        return {
+            "message" : "Logo saved successfully",
+            "backup" : f"s3://my-test-bucket/{new_logoname}",
+            "name": new_logoname
+        }
