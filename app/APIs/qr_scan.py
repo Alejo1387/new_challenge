83c7b96deeb27d1b2411aa95f7eb9b45c4bdb33c
Alejo1387 <trabajoalgoritmos02@gmail.com>
2025-12-24 14:32:51 -0500

    eliminate depends

--- app/APIs/qr_scan.py
+++ app/APIs/qr_scan.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, HTTPException, Depends
+from fastapi import APIRouter, Request, HTTPException
 from app.services.qr_scan import scan_service
 
 router = APIRouter()
